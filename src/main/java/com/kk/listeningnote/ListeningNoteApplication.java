40330f64c49e6d56ce728eeb1e0b392ee04dc876
manone <184355540+manone2077@users.noreply.github.com>
2025-02-10 11:17:56 +0800

    init

---
+++ src/main/java/com/kk/listeningnote/ListeningNoteApplication.java
@@ -0,0 +1,13 @@
+package com.kk.listeningnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeningNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeningNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kk/listeningnote/ListeningNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.listeningnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListeningNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
